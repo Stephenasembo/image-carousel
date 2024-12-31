e0b5f73bbca1aff96917fe820118ce244b1f00d9
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-31 14:21:07 +0300

    Display The First Image When Page Is Loaded

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const slidesFrame = document.querySelector('#slidesFrame');
 
 let imagesArray = slidesFrame.querySelectorAll('img');
 imagesArray = Array.from(imagesArray);
-const imageSrc = imagesArray[0].src;
-const imageAlt = imagesArray[0].alt;
-const imageWidth = imagesArray[0].width;
-const imageHeight = imagesArray[0].height;
-slidesFrame.innerHTML = `<img src = ${imageSrc} alt = ${imageAlt} width = ${imageWidth} height = ${imageHeight}>`;
+function renderInitialImage() {
+  const imageSrc = imagesArray[0].src;
+  const imageAlt = imagesArray[0].alt;
+  const imageWidth = imagesArray[0].width;
+  const imageHeight = imagesArray[0].height;
+  slidesFrame.innerHTML = `<img src = ${imageSrc} alt = ${imageAlt} width = ${imageWidth} height = ${imageHeight}>`;  
+}
+window.addEventListener('load', renderInitialImage);
