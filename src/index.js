5bd768e305ae2057b1b0de36775924f0b7a3a817
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-31 17:03:07 +0300

    Color The Buttons According To Navigation
    Color the button which displays the current image

--- src/index.js
+++ src/index.js
@@ -8,12 +8,41 @@ let imagesArray = slidesFrame.querySelectorAll('img');
 imagesArray = Array.from(imagesArray);
 const imagesNumber = imagesArray.length;
 
+function colorBtn(arr) {
+  arr.forEach((element) => {
+    const button = element;
+    button.style.backgroundColor = 'white';
+  });
+}
+
+(function displayNavigation() {
+  imagesArray.forEach(() => {
+    const imageBtn = document.createElement('button');
+    imageNavigation.appendChild(imageBtn);
+  });
+}());
+
 function displayCurrentImage() {
+  function navigateImages() {
+    let navigationButtons = imageNavigation.querySelectorAll('button');
+    navigationButtons = Array.from(navigationButtons);
+    navigationButtons.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        currentImage = index;
+        displayCurrentImage();
+      });
+    });
+    return navigationButtons;
+  }
+
   const imageSrc = imagesArray[currentImage].src;
   const imageAlt = imagesArray[currentImage].alt;
   const imageWidth = imagesArray[currentImage].width;
   const imageHeight = imagesArray[currentImage].height;
   slidesFrame.innerHTML = `<img src = ${imageSrc} alt = ${imageAlt} width = ${imageWidth} height = ${imageHeight}>`;
+  const buttonsArray = navigateImages();
+  colorBtn(buttonsArray);
+  buttonsArray[currentImage].style.backgroundColor = 'green';
 }
 
 function renderInitialImage() {
@@ -39,22 +68,3 @@ function renderPreviousImage() {
   displayCurrentImage();
 }
 previousImage.addEventListener('click', renderPreviousImage);
-
-function navigateImages() {
-  let navigationButtons = imageNavigation.querySelectorAll('button');
-  navigationButtons = Array.from(navigationButtons);
-  navigationButtons.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-      currentImage = index;
-      displayCurrentImage();
-    });
-  });
-}
-
-(function displayNavigation() {
-  imagesArray.forEach(() => {
-    const imageBtn = document.createElement('button');
-    imageNavigation.appendChild(imageBtn);
-  });
-  navigateImages();
-}());
